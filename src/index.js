3834500bb6905461727c130096fd2f1c42f090a8
AjayDholia <ajaydholia19@gmail.com>
2022-07-25 07:20:18 -0700

    ajay

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Ap from './Ap';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<Ap/>
+);
+
